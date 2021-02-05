6289c71780e18ccaa3c1da8662e68e1d941bd87a
Нина <young_imposer@mail.ru>
2021-02-05 15:08:49 +0300

    Верстала секцию Team

--- assets/build/js/main.js
+++ assets/build/js/main.js
@@ -10,3 +10,34 @@ $('.button').click(function() {
   $('.button').removeClass('button--active');
   $(this).addClass('button--active');
 });
+
+
+// Карусель
+let mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  // autoplay: {
+  //   delay: 3000,
+  // },
+  slideToClickedSlide: true,
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    }
+  }
+
+});
