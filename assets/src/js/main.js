4888934ee0ca7bd6fa302493c10ee696ac1e2439
Нина <young_imposer@mail.ru>
2021-02-07 20:36:43 +0300

    Добавила скрипты для мобильного меню. кнопки вверх

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,4 +1,4 @@
-// ++++++++++ Фильтр для элементов в Portfolio +++++++++++
+// ++++++++++ Фильтр для элементов  mixitup  в Portfolio +++++++++++
 
 var containerEl = document.querySelector('.container');
 var mixer = mixitup(containerEl);
@@ -41,6 +41,53 @@ let mySwiper = new Swiper('.swiper-container', {
   }
 });
 
+// Добавление класса для изменения порядка элементов
+
 $(document).ready(function(){
   $('.blog__item:odd').addClass('reverse');
 });
+
+
+// Плавный переход для меню
+$(function(){
+  $('.menu__link').click(function(){
+    var _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    $('.menu__link').removeClass('menu__link--active');
+    $(this).addClass('menu__link--active');
+    return false;
+  });
+});
+
+
+// Кнопка вверх, -  исчезновение-появление
+
+$(window).scroll(function() {
+  if($(this).scrollTop() != 0) {
+    $('.to-top').fadeIn();
+  } else {
+    $('.to-top').fadeOut();
+  }
+});
+
+// Кнопка вверх, - пролистывание вверх
+$('.to-top').on('click', function() {
+  $('html, body').stop().animate({scrollTop : 0}, 300);
+});
+
+
+//Открытие-закрытие меню
+$(document).ready(function(){
+  if (document.documentElement.clientWidth < 768) {
+    $('.menu').css({"display": "none"});
+    $('.fa-bars').css({"display": "block"});
+
+    $('.button-menu').click(function() {
+      $('.menu').slideToggle(500);
+      $('.fa-bars').toggle();
+      $('.fa-times').toggle();
+    });
+  }
+});
+
+
